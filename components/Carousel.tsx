9c3120ba22644d739d9ae2a8292634817359d03d
Brandon Lindberg <brandon@ekohe.com>
2023-07-07 12:20:44 +0900

    yarn format

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -3,27 +3,32 @@ import Image from 'next/image';
 import 'react-responsive-carousel/lib/styles/carousel.min.css'; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-
 const ImageCarousel = () => {
   return (
-    <Carousel autoPlay interval={2000} infiniteLoop useKeyboardArrows showThumbs={false}>
+    <Carousel
+      autoPlay
+      interval={2000}
+      infiniteLoop
+      useKeyboardArrows
+      showThumbs={false}
+    >
       <div>
-          <img src="/one.jpg" alt="" />
+        <img src="/one.jpg" alt="" />
       </div>
       <div>
-          <img src="/two.jpg" alt="" />
+        <img src="/two.jpg" alt="" />
       </div>
       <div>
-          <img src="/three.jpg" alt="" />
+        <img src="/three.jpg" alt="" />
       </div>
       <div>
-          <img src="/four.jpg" alt="" />
+        <img src="/four.jpg" alt="" />
       </div>
       <div>
-          <img src="/five.jpg" alt="" />
+        <img src="/five.jpg" alt="" />
       </div>
     </Carousel>
-  )
+  );
 };
 
 export default ImageCarousel;
