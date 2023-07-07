eecab536a277570e118edebe7d1acd78a438a0d1
Brandon Lindberg <brandon@ekohe.com>
2023-07-07 15:13:35 +0900

    loading bug fix? for mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,38 +1,59 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import Link from 'next/link';
 import Layout from '../components/Layout';
 import Image from 'next/image';
 import Calendar from '../components/Calendar';
 import ImageCarousel from '../components/Carousel';
 
-const IndexPage = () => (
-  <Layout title="Cultural Fusion Hub">
-    <header>
-      <div
-        style={{ maxHeight: '600px', overflow: 'hidden' }}
-        className="relative w-full"
-      >
-        <ImageCarousel />
-        <div className="absolute bottom-1/4 right-1/2 transform translate-x-1/2">
-          {/* <h1 className="text-white text-4xl">Welcome to The Cultural Fusion Hub</h1> */}
+const IndexPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      if (url === '/') {
+        window.location.reload();
+      }
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Cleanup function
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router]);
+
+  return (
+    <Layout title="Cultural Fusion Hub">
+      <header>
+        <div
+          style={{ maxHeight: '600px', overflow: 'hidden' }}
+          className="relative w-full"
+        >
+          <ImageCarousel />
+          <div className="absolute bottom-1/4 right-1/2 transform translate-x-1/2">
+            {/* <h1 className="text-white text-4xl">Welcome to The Cultural Fusion Hub</h1> */}
+          </div>
+        </div>
+      </header>
+      <main className="height-30vh">
+        <div className="bg-slate-50 p-4">
+          <article className=" p-5 text-justify">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius alias
+            inventore repellendus sit quia architecto praesentium commodi
+            deserunt consectetur. Totam molestias tempora soluta iste, rerum
+            dicta nihil assumenda eveniet? Hic?
+          </article>
+        </div>
+      </main>
+      <div className="p-3">
+        <div className="p-3">
+          <Calendar />
         </div>
       </div>
-    </header>
-    <main className="height-30vh">
-      <div className="bg-slate-50 p-4">
-        <article className=" p-5 text-justify">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eius alias
-          inventore repellendus sit quia architecto praesentium commodi deserunt
-          consectetur. Totam molestias tempora soluta iste, rerum dicta nihil
-          assumenda eveniet? Hic?
-        </article>
-      </div>
-    </main>
-    <div className="p-3">
-      <div className="p-3">
-        <Calendar />
-      </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default IndexPage;
