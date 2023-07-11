3bd08d7d9a65137932f38395cbd84268d17bb98b
Brandon Lindberg <brandon@ekohe.com>
2023-07-11 12:48:22 +0900

    worked on styling

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -37,14 +37,14 @@ const ContactForm = () => {
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             htmlFor="grid-first-name"
           >
-            {/* First Name */}{t('first-name')}
+            {/* First Name */}{t('last-name')}
           </label>
           <input
             className="appearance-none block w-full bg-gray-50 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
             id="grid-first-name"
             type="text"
-            placeholder="Taro"
-            value={formState.firstName}
+            placeholder="Yamada"
+            value={formState.lastName}
             onChange={handleInput}
             name="firstName"
             required
@@ -55,14 +55,14 @@ const ContactForm = () => {
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
             htmlFor="grid-last-name"
           >
-            {/* Last Name */}{t('last-name')}
+            {/* Last Name */}{t('first-name')}
           </label>
           <input
             className="appearance-none block w-full bg-gray-50 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-last-name"
             type="text"
-            placeholder="Yamada"
-            value={formState.lastName}
+            placeholder="Taro"
+            value={formState.firstName}
             onChange={handleInput}
             name="lastName"
             required
