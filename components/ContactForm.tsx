967782a415eab32336a8ad8f492eb2afc8bb7891
Brandon Lindberg <brandon@ekohe.com>
2023-07-11 15:55:58 +0900

    fixed contact form

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,5 +1,5 @@
+import { useTranslation } from "react-i18next";
 import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
 
 const ContactForm = () => {
   const [formState, setFormState] = useState({
@@ -35,38 +35,38 @@ const ContactForm = () => {
         <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-first-name"
+            htmlFor="grid-last-name"
           >
-            {/* First Name */}
+            {/* Last Name */}
             {t('last-name')}
           </label>
           <input
             className="appearance-none block w-full bg-gray-50 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            id="grid-first-name"
+            id="grid-last-name"
             type="text"
             placeholder="Yamada"
             value={formState.lastName}
             onChange={handleInput}
-            name="firstName"
+            name="lastName"
             required
           />
         </div>
         <div className="w-full md:w-1/2 px-3">
           <label
             className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
-            htmlFor="grid-last-name"
+            htmlFor="grid-first-name"
           >
-            {/* Last Name */}
+            {/* First Name */}
             {t('first-name')}
           </label>
           <input
             className="appearance-none block w-full bg-gray-50 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-last-name"
+            id="grid-first-name"
             type="text"
             placeholder="Taro"
             value={formState.firstName}
             onChange={handleInput}
-            name="lastName"
+            name="firstName"
             required
           />
         </div>
