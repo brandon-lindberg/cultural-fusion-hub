5a6d0aa34d66244986530166ec420f19a762ad6e
Brandon Lindberg <brandon@ekohe.com>
2023-07-12 16:37:53 +0900

    favicon

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -41,6 +41,19 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => {
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
         ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="flex flex-col min-h-screen">
         <header className="linear-gradient w-full p-4 flex justify-between items-center">
