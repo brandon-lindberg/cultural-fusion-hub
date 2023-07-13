406e3fecdbef3a20dae7947b1381eaf58871a819
Brandon Lindberg <brandon@ekohe.com>
2023-07-13 14:14:20 +0900

    updated profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,37 +1,41 @@
 import Link from 'next/link';
 import Layout from '../components/Layout';
 import ProfileCard from '../components/ProfileCard';
-import { t } from 'i18next';
+import { useTranslation } from 'react-i18next';
 
-const ProfilePage = () => (
-  <Layout title="Profile">
-    <div className="md:flex md:justify-center md:items-center md:h-screen">
-      <div className="profile m-3">
-        <ProfileCard
-          name="Obikane Yuka"
-          image="/profile_one.jpg"
-          position="Founder"
-          description={t('yuka-description')}
-          instagram="https://www.instagram.com/culturalfusionhub/"
-          // twitter="https://twitter.com/"
-          // linkedin="https://www.linkedin.com/"
-          // facebook="https://www.facebook.com/"
-        />
+const ProfilePage = () => {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <Layout title="Profile">
+      <div className="md:flex md:justify-center md:items-center md:h-screen">
+        <div className="profile m-3">
+          <ProfileCard
+            name="Obikane Yuka"
+            image="/profile_one.jpg"
+            position="Founder"
+            description={t('yuka-description')}
+            instagram="https://www.instagram.com/culturalfusionhub/"
+            // twitter="https://twitter.com/"
+            // linkedin="https://www.linkedin.com/"
+            // facebook="https://www.facebook.com/"
+          />
+        </div>
+        <div className="profile m-3">
+          <ProfileCard
+            name="Saho Petersen"
+            image="/profile_three.jpg"
+            position="Founder"
+            description={t('saho-description')}
+            instagram="https://www.instagram.com/culturalfusionhub/"
+            // twitter="https://twitter.com/"
+            // linkedin="https://www.linkedin.com/"
+            // facebook="https://www.facebook.com/"
+          />
+        </div>
       </div>
-      <div className="profile m-3">
-        <ProfileCard
-          name="Saho Petersen"
-          image="/profile_three.jpg"
-          position="Founder"
-          description={t('saho-description')}
-          instagram="https://www.instagram.com/culturalfusionhub/"
-          // twitter="https://twitter.com/"
-          // linkedin="https://www.linkedin.com/"
-          // facebook="https://www.facebook.com/"
-        />
-      </div>
-    </div>
-  </Layout>
-);
+    </Layout>
+  );
+};
 
 export default ProfilePage;
