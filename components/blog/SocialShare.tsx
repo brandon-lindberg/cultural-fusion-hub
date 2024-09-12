42b22d5855bc9bae6e09090a96a89fb9bf918710
Brandon Lindberg <brandon@ekohe.com>
2024-09-12 14:12:56 +0900

    added copy link button

--- components/blog/SocialShare.tsx
+++ components/blog/SocialShare.tsx
@@ -13,17 +13,28 @@ const SocialShare: React.FC<SocialShareProps> = ({ title, url }) => {
   const linkedinShareUrl = `https://www.linkedin.com/sharing/share-offsite/?url=${encodedUrl}`;
   const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodedUrl}`;
 
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(url).then(() => {
+      alert('Link copied to clipboard!');
+    }).catch(err => {
+      console.error('Failed to copy: ', err);
+    });
+  };
+
   return (
-    <div className="flex space-x-2">
-      <a href={twitterShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-500">
-        <img src="/x-twitter-brands-solid.svg" alt="X" className="w-6 h-6" />
+    <div className="flex space-x-2 items-center mb-2">
+      <a href={twitterShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-500 flex items-center" title="Share on X">
+        <img src="/x-twitter-brands-solid.svg" alt="X" className="w-6 h-6" /> {/* Custom SVG icon */}
       </a>
-      <a href={linkedinShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-700">
+      <a href={linkedinShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-700 flex items-center" title="Share on LinkedIn">
         <i className="fa fa-linkedin fa-lg" aria-hidden="true"></i>
       </a>
-      <a href={facebookShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-600">
+      <a href={facebookShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-600 flex items-center" title="Share on Facebook">
         <i className="fa fa-facebook fa-lg" aria-hidden="true"></i>
       </a>
+      <button onClick={copyToClipboard} className="text-gray-500 flex items-center" title="Copy link">
+        <i className="fa fa-copy fa-lg" aria-hidden="true"></i> {/* Font Awesome copy icon */}
+      </button>
     </div>
   );
 };
