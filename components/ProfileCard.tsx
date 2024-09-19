2ccfc9374707ebca7c9cdb7f072aa80e989db702
Brandon Lindberg <brandon@ekohe.com>
2024-09-19 12:55:44 +0900

    made profile cards responsive for desktop

--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -3,19 +3,21 @@ import Image from 'next/image';
 
 export default function ProfileCard(props) {
   return (
-    <div className="card bg-gray-50 shadow rounded-lg p-6 max-w-xs mx-auto">
+    <div className="card bg-gray-50 shadow rounded-lg p-6 max-w-xs mx-auto md:max-w-md lg:max-w-lg xl:max-w-xl">
       <div className="flex flex-col items-center">
-        <Image
-          src={props.image}
-          alt="pic"
-          height={150}
-          width={150}
-          className="rounded-full"
-          quality={100}
-        />
-        <h1 className="text-lg mt-2">{props.name}</h1>
+        <div className="relative h-40 w-40 md:h-48 md:w-48 lg:h-56 lg:w-56 xl:h-64 xl:w-64">
+          <Image
+            src={props.image}
+            alt="pic"
+            layout="fill"
+            objectFit="cover"
+            className="rounded-full"
+            quality={100}
+          />
+        </div>
+        <h1 className="text-lg mt-2 md:text-xl lg:text-2xl">{props.name}</h1>
       </div>
-      <h3 className="title text-center mb-2">{props.position}</h3>
+      <h3 className="title text-center mb-2 md:text-lg lg:text-xl">{props.position}</h3>
       <div className="border-t border-gray-200 mt-2 mb-4"></div>
 
       <div className="mx-4 text-justify text-center overflow-auto max-h-48 no-scrollbar">
@@ -24,8 +26,8 @@ export default function ProfileCard(props) {
         )}
       </div>
 
-      <div className="flex justify-center gap-2">
-        <a href={props.instagram} target="_blank">
+      <div className="flex justify-center items-center mt-4 relative">
+        <a href={props.instagram} target="_blank" className="z-10">
           <i className="fa fa-instagram text-mainPink"></i>
         </a>
         {/* <a href={props.twitter} target="_blank">
@@ -37,6 +39,7 @@ export default function ProfileCard(props) {
         <a href={props.facebook} target="_blank">
           <i className="fa fa-facebook text-mainPink"></i>
         </a> */}
+        <i className="fa fa-arrow-down text-gray-400 animate-bounce absolute right-0"></i>
       </div>
     </div>
   );
