fd537f6baeb39deb5bc20633fe9bfc6f0d11b9fc
Brandon Lindberg <brandon@ekohe.com>
2024-09-26 16:49:50 +0900

    updated tooltips for social share file

--- components/blog/SocialShare.tsx
+++ components/blog/SocialShare.tsx
@@ -24,18 +24,38 @@ const SocialShare: React.FC<SocialShareProps> = ({ title, url }) => {
 
   return (
     <div className="flex space-x-2 items-center mb-2">
-      <a href={twitterShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-500 flex items-center" title="Share on X">
-        <img src="/x-twitter-brands-solid.svg" alt="X" className="w-6 h-6" /> {/* Custom SVG icon */}
-      </a>
-      <a href={linkedinShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-700 flex items-center" title="Share on LinkedIn">
-        <i className="fa fa-linkedin fa-lg" aria-hidden="true"></i>
-      </a>
-      <a href={facebookShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-600 flex items-center" title="Share on Facebook">
-        <i className="fa fa-facebook fa-lg" aria-hidden="true"></i>
-      </a>
-      <button onClick={copyToClipboard} className="text-gray-500 flex items-center" title="Copy link">
-        <i className="fa fa-copy fa-lg" aria-hidden="true"></i> {/* Font Awesome copy icon */}
-      </button>
+      <div className="relative group">
+        <a href={twitterShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-500 flex items-center" title="Share on X">
+          <img src="/x-twitter-brands-solid.svg" alt="X" className="w-6 h-6" />
+        </a>
+        <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max bg-black text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          Share on X
+        </span>
+      </div>
+      <div className="relative group">
+        <a href={linkedinShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-700 flex items-center" title="Share on LinkedIn">
+          <i className="fa fa-linkedin fa-lg" aria-hidden="true"></i>
+        </a>
+        <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max bg-black text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          Share on LinkedIn
+        </span>
+      </div>
+      <div className="relative group">
+        <a href={facebookShareUrl} target="_blank" rel="noopener noreferrer" className="text-blue-600 flex items-center" title="Share on Facebook">
+          <i className="fa fa-facebook fa-lg" aria-hidden="true"></i>
+        </a>
+        <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max bg-black text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          Share on Facebook
+        </span>
+      </div>
+      <div className="relative group">
+        <button onClick={copyToClipboard} className="text-gray-500 flex items-center" title="Copy link">
+          <i className="fa fa-copy fa-lg" aria-hidden="true"></i>
+        </button>
+        <span className="absolute bottom-full left-1/2 transform -translate-x-1/2 mb-2 w-max bg-black text-white text-xs rounded py-1 px-2 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+          Copy link
+        </span>
+      </div>
     </div>
   );
 };
