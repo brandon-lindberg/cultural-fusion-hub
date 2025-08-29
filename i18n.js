fba5baf1fb299601d197a6f49e234aed24b61767
Brandon Lindberg <brandon@ekohe.com>
2025-08-29 13:33:47 +0900

    quick fix

--- i18n.js
+++ i18n.js
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
 
 import translationEN from './public/locales/en/translation.json';
 import translationJA from './public/locales/ja/translation.json';
@@ -16,12 +15,9 @@ const resources = {
 
 i18n
   .use(initReactI18next)
-  // Apply browser language detection only on the client
-  .use(LanguageDetector)
   .init({
     resources,
     fallbackLng: 'ja',
-    detection: { order: ['path', 'navigator'], lookupFromPathIndex: 0 },
     interpolation: { escapeValue: false },
   });
 // Note: on server, the detector will noop, fallbackLng used
